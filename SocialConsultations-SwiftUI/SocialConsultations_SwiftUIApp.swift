b591e927426d25b3fb2a1283813d6b6b6029a213
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-10-08 15:19:25 +0200

    Initial Commit

---
+++ SocialConsultations-SwiftUI/SocialConsultations_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialConsultations_SwiftUIApp.swift
+//  SocialConsultations-SwiftUI
+//
+//  Created by Paweł Rudnik on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SocialConsultations_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
