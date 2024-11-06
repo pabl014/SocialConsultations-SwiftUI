611f9d7736d43a627112a02f6633cb6bc24f7401
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-11-06 12:19:43 +0100

    Move EditProfileSheet to its own file

---
+++ SocialConsultations-SwiftUI/Screens/Profile/Settings/EditProfileSheet.swift
@@ -0,0 +1,57 @@
+//
+//  EditProfileSheet.swift
+//  SocialConsultations-SwiftUI
+//
+//  Created by Paweł Rudnik on 06/11/2024.
+//
+
+import SwiftUI
+
+struct EditProfileSheet: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    @ObservedObject var viewModel: SettingsViewModel
+    
+    @State private var newName: String = ""
+    @State private var newSurname: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("New name...", text: $newName)
+                TextField("New surname...", text: $newSurname)
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button("Save") {
+                        Task {
+                            await viewModel.updateUserProfileIfNeeded(name: newName, surname: newSurname)
+                        }
+                        dismiss()
+                    }
+                }
+            }
+        }
+        .onAppear {
+            newName = viewModel.user?.name ?? ""
+            newSurname = viewModel.user?.surname ?? ""
+        }
+        .alert(item: $viewModel.errorMessage) { errorMessage in
+            Alert(title: Text("Error"), message: Text(errorMessage.message), dismissButton: .default(Text("OK")))
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        EditProfileSheet(viewModel: SettingsViewModel())
+    }
+}
